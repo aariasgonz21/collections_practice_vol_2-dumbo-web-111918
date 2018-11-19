ec6e97d50540d38c8d808eac72b3e1dd985cc653
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2018-11-19 04:02:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
 # your code goes here
+
+require 'pry'
+def begins_with_r(array)
+  array.all? do |word|
+    word[0] == "r"
+  end
+end
+
+def contain_a(array)
+  array.select do |word|
+    word.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    word.to_s.start_with?("wa")
+  end
+end
+
+def remove_non_strings(array)
+  array.delete_if do |word|
+     !(word.is_a?(String))
+  end  
+end
+
+def count_elements(array)
+  array.each do |hash|
+    hash.group_by()
+  end
+end
